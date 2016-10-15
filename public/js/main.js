aea91dfddafe28168ddccdeffd7569cd0a028dcf
Deanna Baris <deanna.baris@gmail.com>
2016-10-15 13:46:16 -0400

    adding weather data

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,13 @@
+function getWeather() {
+	var url = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&id=4930956&APPID=' + apiKey;
+	$.ajax(url, {
+    success: function (data) {
+      $('.city').text(data.name);
+      $('.temp').text(data.main.temp + ' °F');
+    }
+  });
+	
+}
 
 function myFunction() {
   document.getElementById("demo").innerHTML = "NEWWW PARAGRAPH #javascript #swag";
